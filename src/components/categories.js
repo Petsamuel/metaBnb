590b6288f1553e2dabd9ded66eafb32afa15077a
Bieefilled <65086865+PhetSirmhuel@users.noreply.github.com>
2022-11-14 18:46:18 +0100

    fix: overflow

--- src/components/categories.js
+++ src/components/categories.js
@@ -2,22 +2,29 @@ import React from 'react'
 import Data from "./dataui"
 
 function Categories() {
+    
     return (
         <>
-        <nav class="upper-gap">
-            <ul class="container-flex  list-container">
-                <li>Resturant</li>
-                <li>Cottage</li>
-                <li>Casttle</li>
-                <li>Fantasy City</li>
-                <li>Beach</li>
-                <li>Cabins</li>
-                <li>Off-grid</li>
-                <li>Farm</li>
-            </ul>
-            <input type="text" value="Locate"/>
-        </nav>
-        <div className='container card-container'>
+            <nav className="upper-gap">
+                
+                <div className="mobile-list-wrapper" >
+                    <ul className="container-flex  list-container unordered-list-mobile">
+                   
+                        <li className='list-items'>Resturant</li>
+                        <li>Cottage</li>
+                        <li>Casttle</li>
+                        <li>Fantasy City</li>
+                        <li>Beach</li>
+                        <li>Cabins</li>
+                        <li>Off-grid</li>
+                        <li>Farm</li>
+                    </ul>
+                    <input type="text"  className="locate" Placeholder="Location" />
+                </div>
+               
+            </nav>
+            
+            <div className='card-container'>
                 <div className='card'>
                     {
                         Data.map((card) => (
