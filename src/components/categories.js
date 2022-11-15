8d8df11af5c64db5d5ab3eceefc44cb8625e98b0
Bieefilled <65086865+PhetSirmhuel@users.noreply.github.com>
2022-11-15 11:37:25 +0100

    new changes

--- src/components/categories.js
+++ src/components/categories.js
@@ -2,26 +2,33 @@ import React from 'react'
 import Data from "./dataui"
 
 function Categories() {
-    
+    function Popper(){
+        let list_container=document.querySelector(".list-container");
+        list_container.classList.toggle("list-container")
+        
+    }
     return (
         <>
             <nav className="upper-gap">
-                
-                <div className="mobile-list-wrapper" >
+                <div className=" mobile-list-wrapper" >
+                    <div class="category-menu" onClick={Popper}>
+                        <span className="line"></span>
+                         <span className="line"></span>
+                         <span className="line"></span>
+                        
+                    </div>
                     <ul className="container-flex  list-container unordered-list-mobile">
-                   
-                        <li className='list-items'>Resturant</li>
+                        <li className='list-items'>Restaurant</li>
                         <li>Cottage</li>
-                        <li>Casttle</li>
+                        <li>Castle</li>
                         <li>Fantasy City</li>
                         <li>Beach</li>
                         <li>Cabins</li>
                         <li>Off-grid</li>
                         <li>Farm</li>
                     </ul>
-                    <input type="text"  className="locate" Placeholder="Location" />
+                   <div> <input type="text"  className="locate" Placeholder="Location" /></div>
                 </div>
-               
             </nav>
             
             <div className='card-container'>
