426c218ce9ebbf78563fb0092cf65e51f5cba9e1
Bieefilled <65086865+PhetSirmhuel@users.noreply.github.com>
2022-11-15 19:14:01 +0100

    fix: stars

--- src/components/Stars.js
+++ src/components/Stars.js
@@ -5,41 +5,10 @@ import "../App.css"
 
 function Stars() {
     const [rateHover, setRateHover] = useState(-1);
+
     const highlightRate = (high) => (e) => {
         setRateHover(high);
       };
-
-      const hoverClicked = (rate) => (e) => {
-       rate = rate + 1 
-        
-      };
-
-      function StarLayout() {
-        return (
-          <>
-            <FontAwesomeIcon
-              icon={faStar}
-              className={ rateHover  > 0 ? "orange" : "primary"}
-            />
-            <FontAwesomeIcon
-              icon={faStar}
-              className={ rateHover > 1 ? "orange" : "primary"}
-            />
-            <FontAwesomeIcon
-              icon={faStar}
-              className={ rateHover > 2 ? "orange" : "primary"}
-            />
-            <FontAwesomeIcon
-              icon={faStar}
-              className={rateHover  > 3 ? "orange" : "primary"}
-            />
-            <FontAwesomeIcon
-              icon={faStar}
-              className={ rateHover > 4 ? "orange" : "primary"}
-            />
-          </>
-        );
-      }
 
     return (
         <>
@@ -49,10 +18,11 @@ function Stars() {
                   <FontAwesomeIcon
                     key={index}
                     icon={faStar}
-                    className={rateHover > index - 1 ? "primary" : "orange"}
+                    className={rateHover > index - 1 ? "orange" : "primary"}
+                    
                     onMouseEnter={highlightRate(index)}
                     onMouseLeave={highlightRate(-1)}
-                    onClick={hoverClicked(index)}
+                    
                   />
                 );
               })}
